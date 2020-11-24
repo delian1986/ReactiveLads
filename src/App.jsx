3addb90dcddd750ca2c3e591c8354dc8bbebe36b
Aleksandar Ivanov <aleksandar.g.ivanov@gmail.com>
2020-11-24 21:45:47 +0200

    setting up initial project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import "./App.css";
+import { hot } from "react-hot-loader/root";
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <h1>Homework - Setup react | Aleksandar Ivanov | 24.Nov.2020</h1>
+      </div>
+    );
+  }
+}
+
+export default hot(App);
