0a5bbd12bbd6c9c1f13944c8a5c9a087953f89d8
radkostanev <radkostanev@gmail.com>
2021-01-20 16:01:53 +0200

    initial vrscans fetch is now on mount

--- src/index.js
+++ src/index.js
@@ -2,17 +2,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
-import fetchVrScansThunk from "./services/fetchVrScansThunk";
-import { getLoggedInStatus } from "./selectors";
 import App from "./App";
-
-const state = store.getState();
-const isLoggedIn = getLoggedInStatus(state);
-
-if (isLoggedIn) {
-  //initial loading of vrScans so we don't start with a blank screen
-  store.dispatch(fetchVrScansThunk());
-}
 
 ReactDOM.render(
   <Provider store={store}>
