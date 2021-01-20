216de6a75144b0ba72959a2cba54666e2791f10a
delyan <delyan@smakmedia.com>
2021-01-20 10:56:54 +0200

    location reload to PUBLIC_BASE

--- src/services/handleResponse.js
+++ src/services/handleResponse.js
@@ -4,7 +4,7 @@ const PUBLIC_BASE_PATH = process.env.PUBLIC_BASE_PATH;
 export function handleResponse(response) {
   if (401 === response.status) {
     StorageService.clearUser();
-    location.replace(`${PUBLIC_BASE_PATH}login`);
+    location.replace(PUBLIC_BASE_PATH);
   }
   return response.json();
 }
