d2ba8fe2694a47a423f60ba76cb2805cd31f5d92
delyan <delyan@smakmedia.com>
2021-01-24 16:01:01 +0200

    save presets e2e finalization

--- cypress/integration/SavePresets/index.spec.js
+++ cypress/integration/SavePresets/index.spec.js
@@ -14,17 +14,32 @@ describe("<SavePresets/> e2e", () => {
 
     it("should apply and remove filters", function () {
         cy.wait(2000);
+
+        //filters
         const carPaintType = cy.get("input").eq(3);
         carPaintType.click();
         carPaintType.should("be.checked");
+        const searchBox = cy.get("input[class=\"form-control form-control-sm mb-3\"]");
+        searchBox.type("car");
+        searchBox.should("have.value", "car");
+
+        //template title
         const templateTitleText = "car paint test"+btoa(Math.random()).substr(10, 5);
-        cy.get("input").first().type(templateTitleText);
+        const templateInput = cy.get("input").first();
+        templateInput.type(templateTitleText);
+        templateInput.should("have.value", templateTitleText);
+
+        // add
         cy.get("button[class=\"btn btn-success\"]").click();
         const presetsDropdown =cy.get("select");
         presetsDropdown.contains(templateTitleText);
 
+        // remove
         cy.get("button[class=\"btn btn-danger\"]").click();
+
+        // final assert
         cy.get("select").should("have.value", 0);
-        carPaintType.should("not.be.checked");
+        cy.get("input").eq(3).should("not.be.checked");
+        cy.get("input[class=\"form-control form-control-sm mb-3\"]").should("have.value", "");
     });
 });
