a140517dddca75a249f4e1ffea709a7234df752d
radkostanev <radkostanev@gmail.com>
2021-01-25 14:43:24 +0200

    filters e2e tests

---
+++ cypress/integration/MaterialTypes/index.spec.js
@@ -0,0 +1,43 @@
+describe("<MaterialTypes/> e2e", () => {
+    beforeEach(() => {
+        cy.login();
+        cy.visit("http://localhost:3000/");
+    });
+
+    afterEach(() => {
+        cy.clearLocalStorage();
+    });
+
+    it("<MaterialTypes/> should be loaded", function () {
+        cy.get("h5").eq(2).should("contain", "MaterialTypes");
+    });
+
+    it("should apply and remove material type filters", function () {
+        cy.wait(2000);
+
+        const fabricMaterialType = cy.get("input[type=checkbox]").eq(1);
+        fabricMaterialType.click();
+        fabricMaterialType.should("be.checked");
+
+        fabricMaterialType.click();
+        fabricMaterialType.should("not.be.checked");
+    });
+
+    it("should return no scans", function () {
+        cy.wait(2000);
+
+        const glossyTag = cy.get("input[type=checkbox]").eq(16);
+        glossyTag.click();
+        glossyTag.should("be.checked");
+
+        cy.wait(2000);
+
+        const hologramMaterialType = cy.get("input[type=checkbox]").eq(14);
+        hologramMaterialType.click();
+        hologramMaterialType.should("be.checked");
+
+        cy.wait(2000);
+
+        cy.get("div[class=\"card p-3 overflow-auto h-100\"]").should("contain", "No results.");
+    });
+});
